dcaa6bd6613f503fbedb953847758da8eadbe084
sobhanmondal <mondalsobhan2006@gmail.com>
2025-04-03 17:59:03 +0530

    Create Calculator.java

---
+++ Calculator.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter first number:");
+        double num1 = scanner.nextDouble();
+
+        System.out.println("Enter second number:");
+        double num2 = scanner.nextDouble();
+
+        System.out.println("Choose operation: +, -, *, /");
+        char operation = scanner.next().charAt(0);
+
+        switch (operation) {
+            case '+': System.out.println("Result: " + (num1 + num2)); break;
+            case '-': System.out.println("Result: " + (num1 - num2)); break;
+            case '*': System.out.println("Result: " + (num1 * num2)); break;
+            case '/': System.out.println("Result: " + (num1 / num2)); break;
+            default: System.out.println("Invalid operation");
+        }
+    }
+}
